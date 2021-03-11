7bb307f31497cf72d2ff73c4cd0bb88472be34e9
Knox <knoxfang@outlook.com>
2021-03-11 12:23:56 -0600

    Finised all assgnment 2 code

---
+++ MiniFarm.py
@@ -0,0 +1,26 @@
+# The tuple type
+theFarmcal = ('apple','potato','orange','sheep','cattle','strawberry','blueberry')
+
+# The set type
+theFarmNum = {5,3,87,53,24,2,3}
+
+# Distrube each type of those two variable
+print("Farms have those things: ", theFarmcal)
+print("Those things of following quanlity: ",theFarmNum)
+
+# Comparison operators
+if type(theFarmcal) == tuple:
+    print("theFarmcal is tuple type")
+else:
+    print("theFarmcal isn't tuple type")
+
+# boolean
+x = 65
+y = 54
+print(x == y)
+print(x != y)
+
+
+
+
+
