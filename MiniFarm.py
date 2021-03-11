8299ad288d16fa6f0b373801f86f10608ef1553a
Knox <knoxfang@outlook.com>
2021-03-11 12:27:25 -0600

    add loop part for task 3

--- MiniFarm.py
+++ MiniFarm.py
@@ -4,6 +4,9 @@ theFarmcal = ('apple','potato','orange','sheep','cattle','strawberry','blueberry
 # The set type
 theFarmNum = {5,3,87,53,24,2,3}
 
+# List of all animal
+allAnimal = ["dog", "cat", "beer", "sheep" ]
+
 # Distrube each type of those two variable
 print("Farms have those things: ", theFarmcal)
 print("Those things of following quanlity: ",theFarmNum)
@@ -19,6 +22,11 @@ x = 65
 y = 54
 print(x == y)
 print(x != y)
+
+# loop
+for i in allAnimal:
+    print(i)
+
 
 
 
